2555ba7a20fe20d6f886e81a08861a5f6c5e6ed5
Christian De Zan <dezan2095@gmail.com>
2024-06-09 11:24:19 +0200

    refactor: rename upload file component

--- src/renderer/src/components/UploadFileContainer.tsx
+++ src/renderer/src/components/UploadFileContainer.tsx
@@ -3,11 +3,11 @@ import { styled } from '@mui/material/styles'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload'
 import { Button, Box, Typography } from '@mui/material'
 
-interface UploadFileProps {
+interface UploadFileContainerProps {
   handleFilesUploaded: (file: File[]) => void
 }
 
-export default function UploadFile({ handleFilesUploaded }: UploadFileProps) {
+export default function UploadFileContainer({ handleFilesUploaded }: UploadFileContainerProps) {
   const [isDragging, setIsDragging] = useState(false)
 
   const VisuallyHiddenInput = styled('input')({
@@ -56,12 +56,12 @@ export default function UploadFile({ handleFilesUploaded }: UploadFileProps) {
         onDragLeave={handleDragLeave}
         onDrop={handleDrop}
         sx={{
-          border: isDragging ? '2px dashed #1976d2' : '2px dashed #aaa',
+          border: isDragging ? '2px dashed #5BE0E5' : '2px dashed #aaa',
           padding: '20px',
           borderRadius: '5px',
           textAlign: 'center',
           cursor: 'pointer',
-          bgcolor: isDragging ? '#f0f0f0' : 'inherit'
+          bgcolor: isDragging ? '#001718' : 'inherit'
         }}
       >
         <Button
@@ -74,7 +74,7 @@ export default function UploadFile({ handleFilesUploaded }: UploadFileProps) {
           Upload file
           <VisuallyHiddenInput onChange={handleFileSelect} type="file" multiple />
         </Button>
-        <Typography variant="body1" color="textSecondary" mt={2}>
+        <Typography variant="body1" mt={2}>
           or drag and drop files here
         </Typography>
       </Box>
